fab6a05d653a8a73bcbff3b84865b7a7b117fa58
SohilVekariya <sohil.vekariya@etatvasoft.com>
2024-06-21 18:07:05 +0530

    redux toolkit completed

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Addtodo from "./comonents/AddTodo"
+import Todos from "./comonents/Todos"
+
+function App() {
+
+  return (
+    <>
+      <h1>Learn about redux toolKit</h1>
+      <Addtodo/>
+      <Todos/>
+    </>
+  )
+}
+
+export default App
